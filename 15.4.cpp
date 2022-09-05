db9cb687d95d917edd41fa97be534230189ba629
Nikita <nikitaputintsev@gmail.com>
2022-09-05 16:10:30 +0300

    first commit

---
+++ 15.4.cpp
@@ -0,0 +1,12 @@
+﻿#include "iostream"
+void parity(bool b, int N) {
+	for (int i = b; i <= N; i += 2)
+		std::cout << i << ' ';
+}
+
+int main() { 
+	const int N = 13;
+	parity(false, N);
+
+	return 0;
+}
